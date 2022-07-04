cb2ec5f3279ba3135042eba2cac5258d6597a447
kevinz <kevin.zhang@moredist.com>
2022-07-04 10:21:43 +0800

    update

--- src/demo.ts
+++ src/demo.ts
@@ -11,7 +11,8 @@ async function run() {
   // const q = use("qq");
   // const q = use("sina")
   // const q = use("boc");
-  const q = use("kday");
+  // const q = use("kday");
+  const q = use("ktime");
   const res = await q.real(["688312", "688533"], true);
 
   console.log(res);
